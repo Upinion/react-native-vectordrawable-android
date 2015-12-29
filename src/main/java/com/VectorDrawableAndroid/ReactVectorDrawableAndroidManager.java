d2a6074394549b1c2a678aea1d54bb156fa3dd35
juldiadia <julian.diaz@upinion.com>
2015-12-29 17:31:17 +0100

    Added compatibility with Android 4.0, and Animations for Android < 5.0

--- src/main/java/com/VectorDrawableAndroid/ReactVectorDrawableAndroidManager.java
+++ src/main/java/com/VectorDrawableAndroid/ReactVectorDrawableAndroidManager.java
@@ -3,11 +3,14 @@ package com.upinion.VectorDrawableAndroid;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.graphics.drawable.*;
+import com.wnafee.vector.compat.*;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.animation.ObjectAnimator;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.os.Build;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.BaseViewPropertyApplicator;
@@ -59,7 +62,22 @@ public class ReactVectorDrawableAndroidManager extends ViewGroupManager<Relative
             throw new JavascriptException("No valid resourceName");
 
         ImageView img = new ImageView(view.getContext());
-        img.setImageResource(resourceIdent);
+        Drawable draw;
+        //Fix bug with Android 5.0 crashing
+        if (Build.VERSION.SDK_INT != Build.VERSION_CODES.LOLLIPOP)
+            draw = ResourcesCompat.getDrawable(view.getContext(), resourceIdent);
+        else{
+            try {
+                draw = VectorDrawable.getDrawable(view.getContext(), resourceIdent);
+            } catch (IllegalArgumentException e1) {
+                try {
+                    draw = AnimatedVectorDrawable.getDrawable(view.getContext(), resourceIdent);
+                } catch (IllegalArgumentException e2) {
+                    throw new JavascriptException(e2.toString());
+                }
+            }
+        }
+        img.setImageDrawable(draw);
 
         if(img.getDrawable() instanceof AnimatedVectorDrawable && props.hasKey(PROP_ANI) && props.getArray(PROP_ANI).size() > 0){
             for( int i = 0; i < props.getArray(PROP_ANI).size(); i++) {
@@ -72,8 +90,28 @@ public class ReactVectorDrawableAndroidManager extends ViewGroupManager<Relative
                     Integer resourceAnimation;
                     if( (resourceAnimation = view.getContext().getResources().getIdentifier( props.getArray(PROP_ANI).getMap(i).getString("resourceAnimation"), "anim", view.getContext().getPackageName())) == 0 )
                         throw new JavascriptException("Invalid resourceAnimation");
-                    else{
-                        Animator animation = AnimatorInflater.loadAnimator(view.getContext(), resourceAnimation);
+                    else {
+                        Animator animation;
+                        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP && ((AnimatedVectorDrawable) img.getDrawable()).isPath(targetName)){
+                            Float pathError;
+                            try {
+                                Field auxf = AnimatedVectorDrawable.class.getDeclaredField("mAnimatedVectorState");
+                                auxf.setAccessible(true);
+                                Object privateObject = auxf.get((AnimatedVectorDrawable)img.getDrawable());
+
+                                auxf = privateObject.getClass().getDeclaredField("mVectorDrawable");
+                                auxf.setAccessible(true);
+                                VectorDrawable vdraw = (VectorDrawable)auxf.get(privateObject);
+                                pathError = vdraw.getPixelSize();
+
+                            } catch (Exception e) {
+                                throw new JavascriptException(e.toString());
+                            }
+
+                            animation = PathAnimatorInflater.loadAnimator(view.getContext(), view.getContext().getResources(), null, resourceAnimation, pathError);
+                        }else{
+                            animation = AnimatorInflater.loadAnimator(view.getContext(), resourceAnimation);
+                        }
                         addAnimator((AnimatedVectorDrawable)img.getDrawable(), targetName, animation);
                     }
                 }else if(props.getArray(PROP_ANI).getMap(i).hasKey("propertyName")) {
@@ -103,28 +141,34 @@ public class ReactVectorDrawableAndroidManager extends ViewGroupManager<Relative
             }
         }
         view.addView(img, 0);
-        if(img.getDrawable() instanceof AnimatedVectorDrawable)
+        if(img.getDrawable() instanceof AnimatedVectorDrawable && props.hasKey(PROP_ANI) && props.getArray(PROP_ANI).size() > 0)
             ((Animatable)img.getDrawable()).start();
 
         super.updateView(view, props);
     }
 
     /*
-     * Use reflection for adding an animator to a AnimatedVectorDrawable
+     * Use reflection for adding an animator to an AnimatedVectorDrawable
      */
     private void addAnimator (AnimatedVectorDrawable draw, String targetName, Animator animation) {
         try {
-            Field auxf = AnimatedVectorDrawable.class.getDeclaredField("mAnimatedVectorState");
-            auxf.setAccessible(true);
-            Object privateObject = auxf.get(draw);
-            Class privateClass = privateObject.getClass();
+            //Android 6.0
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Field auxf = AnimatedVectorDrawable.class.getDeclaredField("mAnimatedVectorState");
+                auxf.setAccessible(true);
+                Object privateObject = auxf.get(draw);
+                Class privateClass = privateObject.getClass();
 
-            Method auxM = privateClass.getDeclaredMethod("addTargetAnimator", String.class, Animator.class);
-            auxM.setAccessible(true);
-            auxM.invoke(privateObject, targetName, animation);
-
+                Method auxM = privateClass.getDeclaredMethod("addTargetAnimator", String.class, Animator.class);
+                auxM.setAccessible(true);
+                auxM.invoke(privateObject, targetName, animation);
+            }else {
+                Method auxM = draw.getClass().getDeclaredMethod("setupAnimatorsForTarget", String.class, Animator.class);
+                auxM.setAccessible(true);
+                auxM.invoke(draw, targetName, animation);
+            }
         } catch (Exception e) {
-            throw new JavascriptException(e.getMessage());
+            throw new JavascriptException(e.toString());
         }
     }
 }
