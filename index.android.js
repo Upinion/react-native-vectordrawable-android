dacacae938debc8ea354e62471d915a5c430d66a
Gerben Kuipers <gerben@upinion.com>
2016-01-11 16:52:39 +0100

    Added `...View.propTypes` to the map of propTypes for react native 0.16.0 and up support.

--- index.android.js
+++ index.android.js
@@ -1,8 +1,10 @@
 var { requireNativeComponent, PropTypes } = require('react-native');
+var View = require('View');
 
 var iface = {
     name: 'VectorDrawableView',
     propTypes: {
+        ...View.propTypes,
         resourceName: PropTypes.string,
         animations: PropTypes.array
     }
